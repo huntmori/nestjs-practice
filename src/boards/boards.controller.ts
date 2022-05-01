f3929ce1800bac71407db315dbcb1e41c85e3085
huntmori <huntmori04x22@gmail.com>
2022-05-02 02:02:33 +0900

    add getAllBoards

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+  // private boardsService: BoardsService;
+
+  // constructor(boardsService: BoardsService) {
+  //   this.boardsService = boardsService;
+  // }
+  constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoard() {
+    return this.boardsService.getAllBoards();
+  }
+}
