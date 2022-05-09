a3f03358e7d8c8a3759c004cb9bc8bc2540ad589
huntmori <huntmori04x22@gmail.com>
2022-05-09 19:11:30 +0900

    add config for TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.' + process.env.NODE_ENV + '.env',
+      ignoreEnvFile: false,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.SYNCHRONIZE === 'true',
+      logging: process.env.LOGGING === 'true',
+    }),
+    BoardsModule,
+  ],
 })
 export class AppModule {}
