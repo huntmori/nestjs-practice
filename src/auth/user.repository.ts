592af245173cd43e1f694c73bd4e334bf6272cd8
huntmori <huntmori04x22@gmail.com>
2022-05-16 05:44:42 +0900

    change username unique

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,12 +2,25 @@ import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { EntityRepository } from 'typeorm';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
     const entity = this.create({ username, password });
-    await this.save(entity);
+
+    try {
+      await this.save(entity);
+    } catch (error) {
+      if (error.code === '24505') {
+        throw new ConflictException('username already exist');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
