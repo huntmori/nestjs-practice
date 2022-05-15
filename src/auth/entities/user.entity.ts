2ff7fb416c445b107e22309dcc2901c06001f2a6
huntmori <huntmori04x22@gmail.com>
2022-05-16 08:29:48 +0900

    adjust bcrypt

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -20,7 +20,6 @@ export class User extends BaseEntity {
   username: string;
 
   @Column({
-    length: 20,
     unique: false,
     nullable: false,
   })
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,16 +6,25 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const entity = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const entity = this.create({
+      username: username,
+      password: hashedPassword,
+    });
 
     try {
       await this.save(entity);
     } catch (error) {
+      console.log(error);
       if (error.code === '24505') {
         throw new ConflictException('username already exist');
       } else {
