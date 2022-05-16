eeccbb30ab2afb645da175e8cd09f8292cbc5191
huntmori <huntmori04x22@gmail.com>
2022-05-17 05:12:15 +0900

    implement jwt for login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.PASSWORD_HASH_KEY,
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,23 +3,32 @@ import { UserRepository } from './user.repository';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     return this.userRepository.createUser(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialDto;
     const user = await this.userRepository.findOne({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login-success';
+      // create user token(secret + payload)
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload, {
+        secret: process.env.PASSWORD_HASH_KEY,
+      });
+      return { accessToken };
     } else {
       throw new UnauthorizedException('login-failed');
     }
